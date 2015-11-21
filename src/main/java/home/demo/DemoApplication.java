6d7514265ddaef3d3123fa97adcb85fc1873e014
Sebastien Lambert <Sebastien Lambert>
2015-11-21 18:41:56 +0800

    Hello

--- src/main/java/home/demo/DemoApplication.java
+++ src/main/java/home/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package home;
+package home.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+		System.out.println("Hello");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
--- src/test/java/home/demo/DemoApplicationTests.java
+++ src/test/java/home/demo/DemoApplicationTests.java
@@ -1,8 +1,11 @@
-package home;
+package home.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import home.demo.DemoApplication;
+
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
